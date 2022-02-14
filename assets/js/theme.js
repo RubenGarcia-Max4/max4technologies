21a868afbe8de4b910195d80d69f54a10bc30f7f
Armando <up190316@alumnos.upa.edu.mx>
2022-02-14 13:01:52 -0600

    add another input in the for

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,13 +1,13 @@
 (function($) {
     'use strict';
-	
-	// Mobile Menu
+
+    // Mobile Menu
     $('.mobile-menu nav').meanmenu({
         meanScreenWidth: "990",
         meanMenuContainer: ".mobile-menu",
         onePage: false,
-    });	
-	 // Slider Active
+    });
+    // Slider Active
     $('.slider_list').owlCarousel({
         loop: true,
         autoplay: true,
@@ -32,14 +32,14 @@
                 items: 1
             }
         }
-    })	
+    })
 
-	/*---------------------
+    /*---------------------
     WOW active js 
     --------------------- */
     new WOW().init();
-	
-	/*---------------------
+
+    /*---------------------
     Nivo Slider active js 
     --------------------- */
     $('#mainSlider').nivoSlider({
@@ -57,18 +57,18 @@
         delay: 10,
         time: 1000
     });
-	// sticky
-	var wind = $(window);
-	var sticky = $('#sticky-header');
-	wind.on('scroll', function () {
-		var scroll = wind.scrollTop();
-		if (scroll < 100) {
-			sticky.removeClass('sticky');
-		} else {
-			sticky.addClass('sticky');
-		}
-	});
-	
+    // sticky
+    var wind = $(window);
+    var sticky = $('#sticky-header');
+    wind.on('scroll', function() {
+        var scroll = wind.scrollTop();
+        if (scroll < 100) {
+            sticky.removeClass('sticky');
+        } else {
+            sticky.addClass('sticky');
+        }
+    });
+
     /* Portfolio Isotope  */
     $('.image_load').imagesLoaded(function() {
 
@@ -108,8 +108,8 @@
         };
 
     });
-	
-		    // Venubox
+
+    // Venubox
 
     $('.venobox').venobox({
 
@@ -118,34 +118,34 @@
         infinigall: true
 
     });
-	
-	 // Case Study Active
+
+    // Case Study Active
     $('.case_study_list').owlCarousel({
-        loop: true,
-        autoplay: true,
-        autoplayTimeout: 10000,
-        dots: true,
-        nav: false,
-        navText: ["<i class='fa fa-long-arrow-left''></i>", "<i class='fa fa-long-arrow-right''></i>"],
-        responsive: {
-            0: {
-                items: 1
-            },
-            768: {
-                items: 3
-            },
-            992: {
-                items: 4
-            },
-            1000: {
-                items: 4
-            },
-            1920: {
-                items: 5
+            loop: true,
+            autoplay: true,
+            autoplayTimeout: 10000,
+            dots: true,
+            nav: false,
+            navText: ["<i class='fa fa-long-arrow-left''></i>", "<i class='fa fa-long-arrow-right''></i>"],
+            responsive: {
+                0: {
+                    items: 1
+                },
+                768: {
+                    items: 3
+                },
+                992: {
+                    items: 4
+                },
+                1000: {
+                    items: 4
+                },
+                1920: {
+                    items: 5
+                }
             }
-        }
-    })	
-	 // Case Study Active
+        })
+        // Case Study Active
     $('.case_study_list2').owlCarousel({
         loop: true,
         autoplay: true,
@@ -170,90 +170,90 @@
                 items: 3
             }
         }
-    })	
-	
- // Testimonial Active
-    $('.testimonial_list').owlCarousel({
-        loop: true,
-        autoplay: true,
-        autoplayTimeout: 10000,
-        dots: true,
-        nav: false,
-        navText: ["<i class='fa fa-long-arrow-left''></i>", "<i class='fa fa-long-arrow-right''></i>"],
-        responsive: {
-            0: {
-                items: 1
-            },
-            768: {
-                items: 2
-            },
-            992: {
-                items: 3
-            },
-            1000: {
-                items: 3
-            },
-            1920: {
-                items: 3
-            }
-        }
-    })	
-	 // Testimonial List Two Active
-    $('.testimonial_list2').owlCarousel({
-        loop: true,
-        autoplay: true,
-        autoplayTimeout: 10000,
-        dots: true,
-        nav: false,
-        navText: ["<i class='fa fa-long-arrow-left''></i>", "<i class='fa fa-long-arrow-right''></i>"],
-        responsive: {
-            0: {
-                items: 1
-            },
-            768: {
-                items: 1
-            },
-            992: {
-                items: 1
-            },
-            1000: {
-                items: 1
-            },
-            1920: {
-                items: 1
-            }
-        }
-    })	
- // Brand Active
-    $('.brand_list').owlCarousel({
-        loop: true,
-        autoplay: true,
-        autoplayTimeout: 10000,
-        dots: true,
-        nav: false,
-        navText: ["<i class='fa fa-long-arrow-left''></i>", "<i class='fa fa-long-arrow-right''></i>"],
-        responsive: {
-            0: {
-                items: 1
-            },
-			600: {
-                items: 2
-            },
-            768: {
-                items: 3
-            },
-            992: {
-                items: 5
-            },
-            1000: {
-                items: 5
-            },
-            1920: {
-                items: 5
-            }
-        }
     })
-	// Brand Two
+
+    // Testimonial Active
+    $('.testimonial_list').owlCarousel({
+            loop: true,
+            autoplay: true,
+            autoplayTimeout: 10000,
+            dots: true,
+            nav: false,
+            navText: ["<i class='fa fa-long-arrow-left''></i>", "<i class='fa fa-long-arrow-right''></i>"],
+            responsive: {
+                0: {
+                    items: 1
+                },
+                768: {
+                    items: 2
+                },
+                992: {
+                    items: 3
+                },
+                1000: {
+                    items: 3
+                },
+                1920: {
+                    items: 3
+                }
+            }
+        })
+        // Testimonial List Two Active
+    $('.testimonial_list2').owlCarousel({
+            loop: true,
+            autoplay: true,
+            autoplayTimeout: 10000,
+            dots: true,
+            nav: false,
+            navText: ["<i class='fa fa-long-arrow-left''></i>", "<i class='fa fa-long-arrow-right''></i>"],
+            responsive: {
+                0: {
+                    items: 1
+                },
+                768: {
+                    items: 1
+                },
+                992: {
+                    items: 1
+                },
+                1000: {
+                    items: 1
+                },
+                1920: {
+                    items: 1
+                }
+            }
+        })
+        // Brand Active
+    $('.brand_list').owlCarousel({
+            loop: true,
+            autoplay: true,
+            autoplayTimeout: 10000,
+            dots: true,
+            nav: false,
+            navText: ["<i class='fa fa-long-arrow-left''></i>", "<i class='fa fa-long-arrow-right''></i>"],
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                768: {
+                    items: 3
+                },
+                992: {
+                    items: 5
+                },
+                1000: {
+                    items: 5
+                },
+                1920: {
+                    items: 5
+                }
+            }
+        })
+        // Brand Two
     $('.brand_list2').owlCarousel({
         loop: true,
         autoplay: true,
@@ -265,7 +265,7 @@
             0: {
                 items: 1
             },
-			600: {
+            600: {
                 items: 2
             },
             768: {
@@ -283,7 +283,7 @@
         }
     })
 
-	/*--------------------------
+    /*--------------------------
      scrollUp
     ---------------------------- */
     $.scrollUp({
@@ -292,9 +292,9 @@
         scrollSpeed: 900,
         animation: 'fade'
     });
-/*--
-   One Page Nav
-    ----------------------------------- */
+    /*--
+       One Page Nav
+        ----------------------------------- */
     var top_offset = $('.techno_nav_manu').height() - 100;
     $('.techno_nav_manu .techno_menu .nav_scroll').onePageNav({
         currentClass: 'current',
