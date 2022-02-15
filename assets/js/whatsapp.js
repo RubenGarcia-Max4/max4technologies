5682ac93116a142781e69e188d1d0fb17e077f90
RubenGarcia-Max4 <ruben.garcia@max4technologies.com>
2022-02-15 17:36:55 -0600

    Ajuste Wap

--- assets/js/whatsapp.js
+++ assets/js/whatsapp.js
@@ -3,7 +3,7 @@ function whatsApp(){
     var seller =  Math.floor(Math.random()*1);
     var text=encodeURIComponent('¡Quiero saber más acerca del Elevador Automático de Paneles Solares!');
      switch(seller){
-       case 0: phone='5214491112375'; break;
+       case 0: phone='524491112375'; break;
               }
     window.open('https://api.whatsapp.com/send?phone='+phone+'&text='+text);
 }
